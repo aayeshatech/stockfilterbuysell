5a4ac9ddb27ec7b457c1cb90270e2ee46bca39d7
aayeshatech <99399368+aayeshatech@users.noreply.github.com>
2025-08-01 21:37:34 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Prevent unnecessary operations
+# Force pre-built wheels and skip unnecessary operations
 export PIP_NO_CACHE_DIR=yes
 export PIP_DISABLE_PIP_VERSION_CHECK=1
 
-# Stable installation order
-python -m pip install --upgrade "pip<24.1"  # Lock pip version
-pip install --ignore-installed -r requirements.txt
+# Install in optimal order
+python -m pip install --upgrade "pip<24.1"
+pip install --only-binary :all: --ignore-installed -r requirements.txt
