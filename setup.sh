0f275a126607390573483fc453026cba02dd8af4
aayeshatech <99399368+aayeshatech@users.noreply.github.com>
2025-08-01 21:12:14 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-python -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
+python -m pip install --upgrade pip
+pip install --no-cache-dir --only-binary :all: -r requirements.txt
