cfb07afdfc2271e28be9ad2e285d766a5ec2e3cf
aayeshatech <99399368+aayeshatech@users.noreply.github.com>
2025-08-01 21:13:48 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-python -m pip install --upgrade pip
-pip install --no-cache-dir --only-binary :all: -r requirements.txt
+# Prevent unnecessary reinstalls
+export PIP_NO_CACHE_DIR=true
+export PIP_DISABLE_PIP_VERSION_CHECK=1
+
+# Install in optimal order
+pip install --upgrade pip==24.0  # Lock pip version
+pip install --no-build-isolation -r requirements.txt
