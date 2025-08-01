de0f4f198e6159f1bda9a084f807f9e58ff709a3
aayeshatech <99399368+aayeshatech@users.noreply.github.com>
2025-08-01 21:48:05 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,2 @@
-#!/bin/bash
-# Force pre-built wheels and skip unnecessary operations
-export PIP_NO_CACHE_DIR=yes
-export PIP_DISABLE_PIP_VERSION_CHECK=1
-
-# Install in optimal order
-python -m pip install --upgrade "pip<24.1"
-pip install --only-binary :all: --ignore-installed -r requirements.txt
+# Nuclear option - clean everything
+pip freeze | xargs pip uninstall -y
